3c6a4ab29b9fd73cad2e6b3894a2b53a7213a607
Patryk Jastrzebski <patryk.jastrzebski91@gmail.com>
2017-11-19 14:43:13 +0100

    Move examples (readme.md)

--- youtubeLive/YouTubeLive.php
+++ youtubeLive/YouTubeLive.php
@@ -95,17 +95,5 @@ class YouTubeLive {
 
 }
 
-//-------------------Examples---------------------------
-$liveStream = new YouTubeLive('YOUR_CHANNEL_ID', 'YOUR_API_KEY');
-
-foreach ($liveStream->getUpcommingEventArray() as $row) {
-    echo $row['title'] . '<br/>';
-    echo $row['date'] . '<br/>';
-    echo $row['desc'] . '<br/>';
-    echo $row['img'] . '<br/>';
-    echo $row['channel'] . '<br/>';
-    echo $row['url'] . '<br/><br/>';
-}
-//------------------------------------------------------
 ?>
 
