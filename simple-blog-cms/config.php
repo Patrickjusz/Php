83ab764726d82a23c87303eaf20f53b318584dd8
Patryk Jastrzebski <patryk.jastrzebski91@gmail.com>
2017-11-22 15:26:58 +0100

    Set new password in config.php

--- simple-blog-cms/config.php
+++ simple-blog-cms/config.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = '1234';
+$db_pass = '';
 $db_name = 'db_blog';
 $db_posty_table = 'blog_posty';
 ?>
--- simple-power-point/config.php
+++ simple-power-point/config.php
@@ -1,6 +1,6 @@
 <?php
 $db_adres = "localhost";
 $db_user = "root";
-$db_pass = "1234";
+$db_pass = "";
 $db_name = "db_prezentacja";
 ?>
